7303127fc5656996e03bd23d33f71ce6de68c639
Thorsten Hohage <Hohage@math.uni-goettingen.de>
2025-09-12 11:45:39 +0200

    added new implementation of Theta operator

--- C03/phaseless_passive_ip_ops.py
+++ C03/phaseless_passive_ip_ops.py
@@ -4,7 +4,7 @@ import numpy as np
 import logging
 
 from regpy.operators import Operator
-from regpy.vecsps import UniformGridFcts
+from regpy.vecsps import UniformGridFcts,VectorSpace, DirectSum
     
 class Corr(Operator):
     
@@ -74,6 +74,86 @@ class Tau(Operator):
         first_adj=np.sum(C*self.A.conj().reshape(self.N, self.N, self.k, 1), axis=-2).conj()
         return first_adj+second_adj
         
+class MatrixProductOp(Operator):
+    
+    """Operator mapping a pair of rectangluar matrices (D,E) of the same size to the matrix product D*E^*.
+    D and E may be represent mapping from a tensor space of shape shape_columns to a tensor 
+    space of shape shape_rows such that D and E are tensor of shape shape_rows+shape_columns (+ in the sense of concatenation of tuples).
+    Input:
+        DE:  numpy array with first dimension =2: D=DE[0,:] and E=DE[1,:] 
+        ndim_cols :  integer [default 1] number of dimensions of the domain of D,E as linear mappings
+    Output: matrix product
+        D @ E^T, a tensor of shape shape_col+shape_col
+    This operator is particularly useful if the product of the last ndim_cols dimensions is much smaller than 
+    the product of the other dimensions.
+    _deriv_adjoint and _eval_adjoint can be evaluated even if the matrix product does not fit into storage.
+    """
+    
+    def __init__(self, MatrixSpace,ndim_col=1):
+        if not isinstance(MatrixSpace,VectorSpace):
+            raise TypeError(f'First argument must be a VectorSpace. Was given {MatrixSpace1}')
+        else:
+            self.shape = MatrixSpace.shape
+            dtype = MatrixSpace.dtype
+        if not isinstance(ndim_col,int):
+            raise TypeError('ndim_col must be integer.')
+            self.s = s
+        self.ndim_col = ndim_col
+        self.ndim_row = len(self.shape) - ndim_col
+        self.shape_columns = self.shape[:-ndim_col]
+        self.shape_rows = self.shape[-ndim_col:]
+
+        shape_domain = (2,)+self.shape
+        shape_codomain = self.shape_columns*2      
+        super().__init__(VectorSpace(shape_domain,dtype=dtype), 
+                         VectorSpace(shape_codomain,dtype=dtype), 
+                         linear=False)
+
+    def prod_A_BT(self,A,B):
+        # returns matrix product A * B^T 
+        # (sums over the "column axes" of A and B, which are assumed to be the last ones both in A and B)
+        ax = [np.arange(-self.ndim_col,0,1),np.arange(-self.ndim_col,0,1)]  
+        return np.tensordot(A,B,ax)
+    
+    def prod_GT_A(self,G,A):
+        # returns matrix product G^T * A 
+        # (sums over "row axes", which are assumed to be the first ones both in G and A)
+        ax = [np.arange(0,self.ndim_row),np.arange(0,self.ndim_row)]
+        return np.tensordot(G,A,ax)
+ 
+    def prod_G_A(self,G,A):
+        # returns matrix product G * A 
+        # (sums over "row axes", which are assumed to the  last ones of G and the first ones of A )
+        ax = [np.arange(-self.ndim_row,0,1),np.arange(0,self.ndim_row)]      
+        return np.tensordot(G,A,ax)
+
+    def _eval(self, DE, differentiate=False, adjoint_derivative=True):
+        if differentiate==True:
+            self.DE = DE
+        return self.prod_A_BT(DE[0],DE[1])
+
+    def _derivative(self, dDE):
+        return (self.prod_A_BT(self.DE[0],dDE[1]) 
+              + self.prod_A_BT(dDE[0],    self.DE[1]))
+
+    def _adjoint(self, G):
+        return np.stack((self.prod_G_A(G,self.DE[1]),
+                        self.prod_GT_A(G,self.DE[0])),
+                        axis=0)
+
+    def _eval_adjoint(self, DE):
+        self.DE = DE
+        return np.stack((self.prod_G_A(DE[0], self.prod_GT_A(DE[1],DE[1])),
+                        self.prod_G_A(DE[1], self.prod_GT_A(DE[0],DE[0]))),
+                        axis=0)
+
+    def _deriv_adjoint(self, dDE):
+        return np.stack(self.prod_G_A(self.DE[0], self.prod_GT_A(dDE[1],self.DE[1])) 
+                      + self.prod_G_A(dDE[0],  self.proj_GT_A(self.DE[1],self.DE[1])),
+                        self.prod_G_A(self.DE[1], self.prod_GT_A(dDE[0],self.DE[0])) 
+                      + self.prod_G_A(dDE[1],  self.proj_GT_A(self.DE[0],self.DE[0])),
+                        axis=0)
+ 
 class Theta:
     
     """Implements the standard matrix product DxE -> D E^* as an operator. 
@@ -122,48 +202,6 @@ class Theta:
         res[1, ...]=second.reshape(self.N, self.N, self.k, self.k)
         return res
     
-    
-class Theta_2(Operator):
-    
-    """Mapping of DxE -> D E^*, we just apply _deriv_adjoint and _eval_adjoint
-    """
-    
-    def __init__(self, domain, codomain, N, k):
-        self.N=N
-        self.k=k
-        super().__init__(domain, codomain, linear=False)
-    
-    def _deriv_adjoint(self, DE, dDE):
-        D=DE[0, ...]
-        E=DE[1, ...]
-        dD=dDE[0, ...]
-        dE=dDE[1, ...]
-        first=self._backprop(D, E, D, dE)
-        second=self._backprop(D, E, dD, E)
-        return first+second
-    
-    def _eval_adjoint(self, DE, C, k_C=None):
-        D=DE[0, ...]
-        E=DE[1, ...]
-        C_1=C[0, ...]
-        C_2=C[1, ...]
-        return self._backprop(D, E, C_1, C_2, k_G=k_C)
-    
-    def _backprop(self, DE, G_1, G_2, k_G=None):
-        if k_G is None:
-            k_G=self.k**2
-        D=DE[0, ...]
-        E=DE[1, ...]
-        D=D.reshape(self.N**2, self.k**2)
-        E=E.reshape(self.N**2, self.k**2)
-        G_1=G_1.reshape(self.N**2, k_G)
-        G_2=G_2.reshape(self.N**2, k_G)
-        first=np.dot(G_1, G_2.T.conj().dot(E))
-        second=np.dot(G_2, G_1.T.conj().dot(D))
-        res=np.zeros((2, self.N, self.N, self.k, self.k), dtype=complex)
-        res[0, ...]=first.reshape(self.N, self.N, self.k, self.k)
-        res[1, ...]=second.reshape(self.N, self.N, self.k, self.k)
-        return res
     
 class Mat(Operator):
     
