65e93f703a83068a78d75450bc9e1015e502bc64
Jakob Florian Oberender <j.oberender@math.uni-goettingen.de>
2025-12-05 07:41:44 +0100

    Adapted to current regpy version

--- time_stepping/fokker_planck_time_step.py
+++ time_stepping/fokker_planck_time_step.py
@@ -6,9 +6,8 @@ from matplotlib import pyplot as plt
 from regpy.vecsps.ngsolve import NgsBaseVector
 from regpy.hilbert import L2
 from regpy.solvers.nonlinear import IrgnmCG
-from regpy.solvers import RegularizationSetting
+from regpy.solvers import Setting
 from regpy.stoprules import CountIterations
-
 
 #Currently not fully functional as inversion seems to be non-unique
 
@@ -24,27 +23,22 @@ params=FokkerPlanckOp.create_shared(delta_t=delta_t,a=-10,b=10,n_nodes=101)
 operators=[FokkerPlanckOp(**params) for _ in range(step_N)]
 op=time_dependent_operator_chain(operators,separate_parameter_inputs=False,output_intermediate_solutions=False)
 
+
 sol_exact=ngs.GridFunction(op.domain[0].fes)
 drift=ngs.GridFunction(op.domain[1].fes)
-sol=ngs.GridFunction(op.domain[1].fes)
-# sol_exact.Set(ngs.exp(-0.05*(ngs.x-1)*(ngs.x-1)))
-sol_exact.Set(-0.01*(ngs.x-a)*(ngs.x-b))
-# drift.Set(-0.001*ngs.x*ngs.x*ngs.x)
+sol=ngs.GridFunction(op.domain[0].fes)
+sol_exact.Set(ngs.exp(-0.05*(ngs.x-1)*(ngs.x-1)))
 drift.Set(ngs.CoefficientFunction(0.1))
-
-xsmesh=op.codomain.fes.mesh(xs)
 
 x=op.domain.join(NgsBaseVector(sol_exact.vec),NgsBaseVector(drift.vec))
 exact_data=op(x)
-setting=RegularizationSetting(op,L2,L2)
-solver=IrgnmCG(setting,exact_data,alpha,init=0.1*op.domain.ones())
-stoprule=CountIterations(200)
-plt.ion()
-# sol.vec.data=sol_exact.vec
-# plt.plot(xs,sol(xsmesh))
+setting=Setting(op,L2,L2,data=exact_data,regpar=alpha)
+solver=IrgnmCG(setting,init=0.5*op.domain.ones())
+stoprule=CountIterations(20)
+
+data_fits=[]
 for reco, reco_data in solver.until(stoprule):
-    sol.vec.data=reco[1].vec
-    plt.plot(xs,sol(xsmesh))
-    plt.pause(0.01)
+    data_fits.append(setting.h_codomain.norm(reco_data-exact_data))
+
+plt.plot(data_fits)
 plt.show()
-plt.ioff()
